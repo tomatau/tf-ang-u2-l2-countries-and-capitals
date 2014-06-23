85db84b499aea94afff626357ab8fa19b7bbad0b
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-06-23 15:58:12 +0100

    refactored nav

--- app/body.php
+++ app/body.php
@@ -1,12 +1,14 @@
 <div class="container">
     <header>
-        <nav>
-        </nav>
         <h1>Countries &amp; Capitals</h1>
+        <nav>
+            <a href="./#">Home</a>
+            <a href="./#/countries">Browse Countries</a>
+        </nav>
     </header>
     <div ng-if="isLoading">Loading</div>
     <main ng-view ng-if="!isLoading">
-        
+        <!-- Content Here -->
     </main>
     <footer>
         <a href="./#">Home</a>
